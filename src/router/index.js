aeef3064c179605b7dbfe157d797883d97199362
bangpv <bangpv@gemvietnam.com>
2019-04-18 17:53:52 +0700

    vue call api using Axios

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,23 @@
 import Vue from 'vue'
 import Router from 'vue-router'
 import HelloWorld from '@/components/HelloWorld'
+import PriceIndex from '@/components/PriceIndex'
 
 Vue.use(Router)
 
 export default new Router({
+  mode: 'history',
+  hash: false,
   routes: [
     {
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+      path: '/price',
+      name: 'PriceIndex',
+      component: PriceIndex
     }
   ]
 })
